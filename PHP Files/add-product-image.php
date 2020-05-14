642cab4057e05d203018e1067040e91a2b6647a0
jbcostan <35471042+jbcostan@users.noreply.github.com>
2020-05-13 20:30:20 -0400

    Create add-product-image.php

---
+++ PHP Files/add-product-image.php
@@ -0,0 +1,70 @@
+<?php
+
+session_start();
+$currentuser = $_SESSION["username"];
+
+if($currentuser == null || $currentuser == ""){
+ echo "<p> Signed in as: Guest </p>";
+}
+else{
+ echo "<p>Signed in as: " . $currentuser . "</p>";
+
+}
+?>
+
+<?php
+
+// MySQL Connection Information
+$servername = "172.105.27.17";
+$username = "ugate";
+$password = "uwind";
+$dbname = "4150";
+
+// POST variables
+$pname = ($_POST["pname"]);
+$price = ($_POST["price"]);
+$category = ($_POST["category"]);
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// prepare and bind for Product Insertion
+try{
+    $stmt = $conn->prepare("INSERT INTO PRODUCT (`pid`, `pname`, `price`, `category`, `image`) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $sqlpid, $sqlpname, $sqlprice, $sqlcat, $sqlimage);
+
+    // set parameters and execute
+    $sqlpid = NULL;
+    $sqlpname = $pname;
+    $sqlprice = $price;
+    $sqlcat = $category;
+    $sqlimage = "";
+
+    $stmt->execute();
+    //echo "New records created successfully";
+}
+catch (Exception $e) {
+    echo $e->getMessage();
+}
+catch (InvalidArgumentException $e) {
+    echo $e->getMessage();
+}
+$stmt->close();
+
+
+// Put image upload element here
+echo "<h1>Step 2: Adding an Image</h1>
+   <form action='http://www.costanj.myweb.cs.uwindsor.ca/added-product-successfully' method='post' enctype='multipart/form-data'>
+    Select image to upload:
+    <input type='file' name='fileToUpload' id='fileToUpload'>
+    <input type='submit' value='Upload Image & Add Product' name='submit'>
+    </form>";
+
+$conn->close();
+
+
+?>
